5d214613ffb2b36993de2eaba9bd4774879033ba
Dmytro Prontenko <136347904+dmytro-prontenko@users.noreply.github.com>
2025-01-02 20:49:42 +0100

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+  effect: "slide",
+  slidesPerView: 1,
+  spaceBetween: 10,
+});
